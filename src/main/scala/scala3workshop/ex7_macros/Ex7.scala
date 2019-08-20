e249097558a79d88f8638666ba3307b8da59b564
Ken Scambler <ken.scambler@gmail.com>
2019-08-21 00:46:53 +1000

    Tipping toe in the water of macro stuff

--- src/main/scala/scala3workshop/ex7_macros/Ex7.scala
+++ src/main/scala/scala3workshop/ex7_macros/Ex7.scala
@@ -1,12 +1,9 @@
 package scala3workshop.ex7_macros
 
+import scala.compiletime._
+import scala.quoted._
+import scala.tasty.Reflection
 
-// There is a new `inline` keyword, which forces (not recommends - forces!) the compiler to recompile 
-// the code inline at every use-site.
-//
-// This exists for 2 reasons:
-// 1. Advanced users can, with care, optimise code to remove indirection costs.
-// 2. The inline functionality enables Scala 3's macro system.
 
 // To experiment with the code in this chapter, either run `console` in sbt and 
 // scala> import scala3workshop.ex7_macros._
@@ -18,4 +15,38 @@ object Ex7 {
   }
 }
 
+
+inline def macroIf[A, B](inline cond: Boolean, ifTrue: => A, ifFalse: => B) <: Any = 
+  ${ macroIfImpl(cond, 'ifTrue, 'ifFalse) }
+
+def macroIfImpl[A: Type, B: Type](
+    cond: Boolean, 
+    ifTrue: Expr[A], 
+    ifFalse: Expr[B]) given QuoteContext: Expr[Any] = {
+
+  if (cond) ifTrue else ifFalse
+}
+
+inline def swap[A, B](tuple: => (A,B)): (B, A) = 
+ ${ swapExpr('tuple) }
+
+private def swapExpr[A: Type, B: Type](tuple: Expr[(A,B)]) given QuoteContext: Expr[(B, A)] = {
+  tuple match {
+    case '{ ($x1, $x2) } => '{ ($x2, $x1) }
+    case _ => '{ $tuple.swap }
+  }
+}
+
+object Const {
+  def unapply[T](expr: Expr[T]) given (qc: QuoteContext): Option[T] = {
+    import qc.tasty._
+
+    def rec(tree: Term): Option[T] = tree match {
+      case Literal(c) => Some(c.value.asInstanceOf[T])
+      case Block(Nil, e) => rec(e)
+      case _ => None
+    }
+    rec(expr.unseal)
+  }
+}
 // Ex. 7.1
